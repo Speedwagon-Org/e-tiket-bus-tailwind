613f61c39e8acaf687d9fd53a6f8e109868c352f
Arif Jagad Mulya <arifjagad34@gmail.com>
2022-12-28 12:02:13 +0700

    Fixed page not found 404

--- src/user/pages/PageNotFound.jsx
+++ src/user/pages/PageNotFound.jsx
@@ -1,7 +1,34 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+/* import assets */
+import img_404 from "../../assets/images/img_404.svg";
 
 export default class PageNotFound extends Component {
   render() {
-    return <div>PageNotFound</div>;
+    return (
+      <>
+        <section className="lg:mx-40 m-6 lg:my-10">
+          <div className="grid justify-items-center gap-6">
+            <img
+              src={img_404}
+              className="w-3/4 lg:w-3/6 place-self-center"
+              alt="img faq"
+            />
+            <h4 className="text-center font-semibold text-xl">
+              Maaf, halaman yang anda cari tidak ditemukan.
+            </h4>
+            <div>
+              <Link
+                to="/about"
+                className="btn bg-sky-800 px-4 py-2 text-white rounded-md mt-4"
+              >
+                home
+              </Link>
+            </div>
+          </div>
+        </section>
+      </>
+    );
   }
 }
