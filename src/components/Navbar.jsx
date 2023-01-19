11bc918815a0bf06fef045dfe7241003ccf19e5a
Tito Ardimam Rahman <48154253+TitoRahman@users.noreply.github.com>
2023-01-19 22:19:57 +0700

    User Auth File init

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Home from "../user/pages/Home";
 import PesanTiket from "../user/pages/PesanTiket";
 import Bantuan from "../user/pages/Bantuan";
-import Login from "../user/pages/LoginRegis";
+import LoginRegis from "../user/pages/LoginRegis";
 import Blog from "../user/pages/Blog";
 import PageNotFound from "../user/pages/PageNotFound";
 import Dashboard from "../admin/pages/Dashboard";
@@ -20,13 +20,15 @@ import {
 
 import { Disclosure } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Register from "../user/pages/components/Register";
+import Login from "../user/pages/components/Login";
 
 const navigation = [
   { name: "Home", to: "/" },
   { name: "Pesan Tiket", to: "/pesan-tiket" },
   { name: "Bantuan", to: "/bantuan" },
   { name: "Blog", to: "/blog" },
-  { name: "Login", to: "/login" },
+  { name: "Login", to: "/account/login" },
 ];
 
 class Navbar extends Component {
@@ -117,7 +119,10 @@ class Navbar extends Component {
             <Route path="/pesan-tiket" element={<PesanTiket />} />
             <Route path="/bantuan" element={<Bantuan />} />
             <Route path="/blog" element={<Blog />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/account" element={<LoginRegis />}>
+              <Route path={"register"} element = {<Register/>}/>
+              <Route path={"login"} element = {<Login/>}/>
+            </Route>
             <Route path="*" element={<PageNotFound />} />
             <Route path="admin">
               <Route path="dashboard" element={<Dashboard />} />
--- src/user/pages/LoginRegis.jsx
+++ src/user/pages/LoginRegis.jsx
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
-
-export default class LoginRegis extends Component {
-  render() {
-    return <div>LoginRegis</div>;
-  }
+import React from 'react'
+export default function LoginRegis() {
+  let currentRoute = window.location.href.split('/')[window.location.href.split('/').length - 1]
+  return (
+    <div>This is login and register container</div>
+  )
 }
+
